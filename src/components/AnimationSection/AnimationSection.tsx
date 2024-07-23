9071c5cd08fd68f6a25f291a40453061c8d16efe
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-23 19:40:02 +0900

    ✨ Feat: 애니메이션섹션 컴포넌트 구현

---
+++ src/components/AnimationSection/AnimationSection.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { ReactNode, useEffect, useState, useRef } from 'react';
+import { throttle } from 'lodash';
+
+function AnimationSection({ children, className }: { children: ReactNode; className?: string }) {
+  const [, setWheelStep] = useState(0);
+  const sectionsRef = useRef<HTMLDivElement | null>(null);
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  const handleWheelEvent: EventListener = throttle(
+    (e: Event) => {
+      const wheelEvent = e as unknown as WheelEvent;
+      if (timer.current) {
+        return;
+      }
+
+      if (wheelEvent.deltaY > 0) {
+        downWheel();
+        return;
+      }
+      upWheel();
+    },
+    1000,
+    { leading: true, trailing: false },
+  );
+
+  const upWheel = () => {
+    setWheelStep((prev) => prev - 1);
+    if (sectionsRef.current) {
+      sectionsRef.current.scrollBy({
+        left: 0,
+        top: -window.innerHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const downWheel = () => {
+    setWheelStep((prev) => prev + 1);
+    if (sectionsRef.current) {
+      sectionsRef.current.scrollBy({
+        left: 0,
+        top: window.innerHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleWheelEvent);
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      window.removeEventListener('wheel', handleWheelEvent);
+      document.body.classList.remove('overflow-hidden');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <main ref={sectionsRef} className={className}>
+      {children}
+    </main>
+  );
+}
+
+export default AnimationSection;
