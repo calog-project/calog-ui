e824f215bca5b184d196cce047185333567aa62f
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-30 18:58:25 +0900

    ✨ Feat: ESC 키로 모달을 닫기 위한 useHandleModalClick hook 구현

---
+++ src/hooks/useHandleModalClick.ts
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from 'react';
+
+const useHandleModalClick = (modalRef: RefObject<HTMLDivElement>, handleModalClose: () => void) => {
+  useEffect(() => {
+    // ESC keydown => Modal Close
+    const handleKeyDownEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        handleModalClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDownEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDownEsc);
+    };
+  }, [modalRef, handleModalClose]);
+};
+
+export default useHandleModalClick;
