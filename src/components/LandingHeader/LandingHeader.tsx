156d5f8a56decb6c720b91c4bd149b6ac0720021
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-30 19:02:09 +0900

    ✨ Feat: 헤더에 예시 모달 및 버튼 추가

--- src/components/LandingHeader/LandingHeader.tsx
+++ src/components/LandingHeader/LandingHeader.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import Link from 'next/link';
 import Button from '../Button/Button';
+import ExampleModal from '../ExampleModal/ExampleModal';
+import useModal from '@/hooks/useModal';
 
 const LandingHeader = () => {
+  const {
+    openModal: exampleOpenModal,
+    handleModalClose: exampleModalClose,
+    handleModalOpen: exampleModalOpen,
+  } = useModal();
+
   return (
-    <header className="fixed top-0 right-0 left-0 bg-white shadow-custom z-sticky py-2">
+    <header className="fixed top-0 right-0 left-0 bg-white shadow-custom py-2 z-50">
       <div className="relative flex items-center h-[100px] m-0 mx-auto max-w-[1400px] justify-between">
         <Link href="/" className="text-[36px] font-bold">
           Calog
@@ -16,6 +26,16 @@ const LandingHeader = () => {
           </Button>
           <Button buttonSize="normal" bgColor="ghost" className="w-[110px] h-[42px]">
             로그인
+          </Button>
+
+          {/* 예시를 위한 모달 및 버튼  나중에 지울것 */}
+          <ExampleModal openModal={exampleOpenModal} handleModalClose={exampleModalClose} />
+          <Button
+            buttonSize="normal"
+            bgColor="filled"
+            className="w-[110px] h-[42px] bg-blue-33"
+            onClick={exampleModalOpen}>
+            예시모달
           </Button>
         </div>
       </div>
