bb172a1546b317f5a5d264bd439ad0136ccc0561
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-07-30 18:59:47 +0900

    ✨ Feat: 모달을 별도의 DOM 노드에 렌더링하기 위한 ModalPortal 구현

---
+++ src/utils/modalPortal.ts
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+
+interface ModalPortalProps {
+  children: ReactNode;
+}
+
+const ModalPortal = ({ children }: ModalPortalProps) => {
+  if (typeof window !== 'undefined') {
+    const modalContainer = document.getElementById('modal');
+
+    if (modalContainer) {
+      return ReactDOM.createPortal(children, modalContainer);
+    }
+  }
+  return null;
+};
+
+export default ModalPortal;
