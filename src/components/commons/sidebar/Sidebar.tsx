340f937664471b9596e9c67aaec1c78f4c817f1b
imkrmin <zum2n2@gmail.com>
2024-08-13 10:43:40 +0900

    ✨Feat: Sidebar 컴포넌트 UI 구현 및 main 페이지 메타태그 설정

---
+++ src/components/commons/sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+import { HiMenuAlt3 } from 'react-icons/hi';
+import Menu from './Menu';
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div
+      className={`h-full m-10 bg-white rounded-lg shadow ${isOpen ? 'w-[200px]' : 'w-[55px]'} duration-500 text-gray-900 px-4`}>
+      <div className={`py-3 flex mt-5 ${isOpen ? 'justify-end' : 'justify-center'}`}>
+        <HiMenuAlt3 size={25} className="cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+      </div>
+      <Menu isOpen={isOpen} />
+    </div>
+  );
+};
+
+export default Sidebar;
