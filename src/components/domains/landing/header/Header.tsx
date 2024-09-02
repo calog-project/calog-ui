b6a55d7c36411c894d85811a3cd76090aaef4b71
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-09-02 13:10:16 +0900

    🐛 Fix: 폴더구조 변경

---
+++ src/components/domains/landing/header/Header.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Link from 'next/link';
+import useModal from '@/hooks/useModal';
+import Button from '@/components/commons/button/Button';
+import ExampleModal from '../exampleModal/ExampleModal';
+
+const Header = () => {
+  const {
+    openModal: exampleOpenModal,
+    handleModalClose: exampleModalClose,
+    handleModalOpen: exampleModalOpen,
+  } = useModal();
+
+  return (
+    <header className="fixed top-0 right-0 left-0 bg-white shadow-custom py-2 z-50">
+      <div className="relative flex items-center h-[100px] m-0 mx-auto max-w-[1400px] justify-between">
+        <Link href="/" className="text-[36px] font-bold">
+          Calog
+        </Link>
+
+        <div className="flex gap-3">
+          {/* 버튼 공통컴포넌트 만든 후 적용 */}
+          <Button buttonSize="normal" bgColor="filled" className="w-[110px] h-[42px] bg-blue-33">
+            회원가입
+          </Button>
+          <Button buttonSize="normal" bgColor="ghost" className="w-[110px] h-[42px]">
+            로그인
+          </Button>
+
+          {/* 예시를 위한 모달 및 버튼  나중에 지울것 */}
+          <ExampleModal openModal={exampleOpenModal} handleModalClose={exampleModalClose} />
+          <Button
+            buttonSize="normal"
+            bgColor="filled"
+            className="w-[110px] h-[42px] bg-blue-33"
+            onClick={exampleModalOpen}>
+            예시모달
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
