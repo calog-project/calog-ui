7a40259f4626057ab7089ef8e1c09abf61455ecc
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-10-08 08:44:17 +0900

    ✨ Feat: AddScheduleButton 추가

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from '@/components/commons/sidebar/Sidebar';
+import AddScheduleButton from '@/components/domains/main/addSchedule/AddScheduleButton';
 
 export default function MainPage() {
   return (
-    <section className="h-dvh flex">
+    <section className="h-dvh flex relative">
       <Sidebar />
+      <AddScheduleButton />
     </section>
   );
 }
