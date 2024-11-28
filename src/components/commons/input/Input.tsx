813bac6fd05c5ce0427b9fca87ffeb26caa27319
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-11-28 10:47:14 +0900

    💄 Design: input 컴포넌트 UI 수정

--- src/components/commons/input/Input.tsx
+++ src/components/commons/input/Input.tsx
@@ -20,7 +20,7 @@ const Input = ({ title, inputSize, className, register, type, name, placeholder,
   const inputClass = twMerge(inputClasses[inputSize], className);
   return (
     <div className="flex flex-col">
-      <label htmlFor={name} className="mb-2 text-xl font-semibold">
+      <label htmlFor={name} className="mb-2 text-[20px] font-semibold">
         {title}
       </label>
       <input
@@ -32,7 +32,7 @@ const Input = ({ title, inputSize, className, register, type, name, placeholder,
         onChange={onChange}
         className={`${inputClass} h-[50px] rounded-lg px-4 py-3 border border-gray-d9 focus:border-gray-98`}
       />
-      <div>{error?.message && <span className="text-sm text-red">{error.message}</span>}</div>
+      <div>{error?.message && <span className="text-[14px] text-red">{error.message}</span>}</div>
     </div>
   );
 };
