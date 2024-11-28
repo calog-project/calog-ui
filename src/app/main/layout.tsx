11564c65aa6c983a24be30792112663c38992a9b
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-11-28 14:31:49 +0900

    ♻️ Refactor: main layout html, body -> div

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -11,9 +11,5 @@ export default function MainPageLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ko">
-      <body>{children}</body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
