b8b1fe568eaa9985c6c467e03e0ae53b0065d423
imkrmin <zum2n2@gmail.com>
2024-12-08 21:57:16 +0900

    🐛 Fix: 모달 레이아웃 수정 및 AddSchedule 컴포넌트 반영

--- src/components/domains/main/addSchedule/AddScheduleModal.tsx
+++ src/components/domains/main/addSchedule/AddScheduleModal.tsx
@@ -12,7 +12,10 @@ const AddScheduleModal = ({ openModal, handleModalClose }: AddScheduleModalProps
   }
 
   return (
-    <Modal openModal={openModal} handleModalClose={handleModalClose} className="w-[670px] p-[30px]">
+    <Modal
+      openModal={openModal}
+      handleModalClose={handleModalClose}
+      className="w-[670px] h-full p-[30px] overflow-y-scroll">
       <AddScheduleForm handleModalClose={handleModalClose} />
     </Modal>
   );
