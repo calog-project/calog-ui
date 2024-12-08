bd49720aba98c373117c3a36fd2fbfac9c28f1b3
imkrmin <zum2n2@gmail.com>
2024-12-08 17:00:42 +0900

    🎨 Design: Category 컴포넌트 UI 구현 및 MiniCalendar 아이콘 변경

---
+++ src/components/domains/main/category/Category.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { MdArrowLeft, MdArrowRight } from 'react-icons/md';
+import { IoIosMore } from 'react-icons/io';
+import { CgAddR } from 'react-icons/cg';
+import { usePagination } from '@/hooks/usePagination';
+
+const defaultCategories = ['새로운 카테고리', '기본', '공유'];
+const customCategories = ['캘로그 회의', '술', '운동', '공부', '여행'];
+
+const Category = () => {
+  const ITEMS_PER_PAGE = 3;
+
+  const { currentItems, currentPage, totalPages, goToNextPage, goToPrevPage } = usePagination(
+    customCategories,
+    ITEMS_PER_PAGE,
+  );
+
+  return (
+    <div className="flex flex-col gap-[20px] w-full h-[250px] rounded-[8px] border border-gray-89 py-5 px-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-[20px] font-bold">카테고리</h1>
+        <div className="flex items-center gap-10">
+          <MdArrowLeft
+            className={`bg-white rounded-full border border-blue-33 text-[30px] text-blue-33  ${
+              currentPage === 0 ? 'border-gray-78 text-gray-78 cursor-not-allowed' : 'cursor-pointer'
+            }`}
+            onClick={goToPrevPage}
+          />
+          <MdArrowRight
+            className={`bg-white rounded-full border border-blue-33 text-[30px] text-blue-33  ${
+              currentPage === totalPages - 1 ? 'border-gray-78 text-gray-78 cursor-not-allowed' : 'cursor-pointer'
+            }`}
+            onClick={goToNextPage}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-5 w-full">
+        <div className="grid grid-cols-3 grid-rows-2 gap-[20px] w-full">
+          {defaultCategories.map((category, index) => (
+            <div
+              key={index}
+              className={`flex justify-center items-center w-full h-[70px] bg-white border border-gray-98 text-black-17 rounded-[10px] text-center font-medium gap-[5px] cursor-pointer`}>
+              {category}
+              {category === '새로운 카테고리' && <CgAddR className="text-blue-33" />}
+            </div>
+          ))}
+          {currentItems.map((category, index) => (
+            <div
+              key={index}
+              className="relative flex justify-center items-center w-full h-[70px] bg-white border border-gray-98 text-black-17 rounded-[10px] text-center font-medium gap-[5px] cursor-pointer">
+              {category}
+              <button className="absolute right-4 top-1 text-black">
+                <IoIosMore className="w-8 h-8 font-bold" />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
