59d4d5ff601696df4463519d3c8e8d59e910542a
junhyeon0218 <junhyeon0218@daum.net>
2024-12-12 18:05:32 +0900

    ✨ Feat: 이미지 최적화 기능 개발'

---
+++ src/app/api/sharpjs/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+import sharp from 'sharp';
+
+export async function POST(request: Request) {
+  const data = await request.formData();
+  const file = data.get('file') as File;
+
+  if (!file) {
+    return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  let compressedBuffer = await sharp(buffer)
+    .resize(512, 512, { fit: 'inside' })
+    .webp({ quality: 80 }) // 초기 품질 설정
+    .toBuffer();
+
+  // 0.5MB 이하로 압축
+  const maxSize = 0.5 * 1024 * 1024; // 0.5MB
+  let quality = 80;
+
+  while (compressedBuffer.length > maxSize && quality > 10) {
+    quality -= 10;
+    compressedBuffer = await sharp(buffer).resize(512, 512, { fit: 'inside' }).webp({ quality }).toBuffer();
+  }
+
+  const compressedFile = new Blob([compressedBuffer], { type: 'image/webp' });
+  const compressedFileUrl = URL.createObjectURL(compressedFile);
+
+  return NextResponse.json({ url: compressedFileUrl });
+}
