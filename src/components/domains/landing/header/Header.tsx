deb13305accafab6c9075fa4bbc3a1189ce529c8
junhyeon0218 <junhyeon0218@daum.net>
2024-12-13 18:28:56 +0900

    📦️ Chore: 회원가입 링크 추c가 및 헤더 높이 수정

--- src/components/domains/landing/header/Header.tsx
+++ src/components/domains/landing/header/Header.tsx
@@ -6,13 +6,13 @@ import Button from '@/components/commons/button/Button';
 const Header = () => {
   return (
     <header className="fixed top-0 right-0 left-0 bg-white shadow-custom py-2 z-50">
-      <div className="relative flex items-center h-[100px] m-0 mx-auto max-w-[1400px] justify-between">
-        <Link href="/" className="text-4xl font-bold">
+      <div className="relative flex items-center h-[64px] m-0 mx-auto max-w-[1400px] justify-between">
+        <Link href="/" className="text-[36px] font-bold">
           Calog
         </Link>
 
         <div className="flex gap-3">
-          <Link href="/">
+          <Link href="/register">
             <Button buttonSize="normal" bgColor="filled" className="w-[110px] h-[42px] bg-blue-33 text-xl">
               회원가입
             </Button>
