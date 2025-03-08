7fe907bf4f6a7152147e551e2e20aa4ddedc4f9c
imkrmin <zum2n2@gmail.com>
2025-03-08 14:39:37 +0900

    ✨ Feat: Schedule 수정 기능 구현

--- src/actions/schedule.ts
+++ src/actions/schedule.ts
@@ -73,3 +73,47 @@ export const getScheduleDetail = async (scheduleId: number) => {
     alert('일정 상세 정보를 불러오는 데 실패하였습니다.');
   }
 };
+
+export const editSchedule = async (
+  scheduleId: number,
+  data: TSchedule,
+  joiner: string[],
+  startDateTime: string,
+  endDateTime: string,
+  categoryId: string,
+) => {
+  try {
+    const session = await getSession();
+    if (!session || !session.user || !session.user.id) {
+      throw new Error('로그인된 사용자가 아닙니다.');
+    }
+
+    const requestData = {
+      author: session?.user.id,
+      title: data.title,
+      start: startDateTime,
+      end: endDateTime,
+      categoryId: Number(categoryId),
+      joiner: joiner,
+      description: data.description,
+    };
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/schedule/${scheduleId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${session?.accessToken}`,
+      },
+      body: JSON.stringify(requestData),
+    });
+
+    if (!response.ok) {
+      throw new Error('일정 수정 실패');
+    }
+
+    await response.json();
+  } catch (error) {
+    console.error('일정 수정 에러', error);
+    alert('일정 수정을 실패하였습니다.');
+  }
+};
