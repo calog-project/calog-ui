d7de9e73ef3a6b50145e165e2a9235f8eb50659a
junhyeon0218 <junhyeon0218@daum.net>
2025-03-16 12:50:41 +0900

    📦️ Chore: authOption경로수정

--- src/actions/calendar.ts
+++ src/actions/calendar.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/lib/authOption';
 import { TCalendar } from '@/types/calendar';
 import { TCategory } from '@/types/category';
 import { TSchedule } from '@/types/schedule';
