bbef6a79f599ab42f5bddfeb3612cdabd8e03fd3
junhyeon0218 <junhyeon0218@daum.net>
2025-03-16 12:50:41 +0900

    📦️ Chore: authOption경로수정

--- src/actions/category.ts
+++ src/actions/category.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/api/auth/[...nextauth]/lib/authOption';
 import { TCategory } from '@/types/category';
 import { getServerSession } from 'next-auth';
 import { revalidatePath } from 'next/cache';
