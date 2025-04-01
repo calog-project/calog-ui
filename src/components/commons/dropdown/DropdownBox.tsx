cde5de9a7316a20df14d750d4da1058932fd0451
junhyeon0218 <junhyeon0218@daum.net>
2025-04-02 00:46:45 +0900

    📦️ Chore: 의존성배열 복구

--- src/components/commons/dropdown/DropdownBox.tsx
+++ src/components/commons/dropdown/DropdownBox.tsx
@@ -69,7 +69,7 @@ const DropdownBox = ({ dataType, title, className, dropdownClassName, value, onC
 
   useEffect(() => {
     setItem(dataType === 'category' ? findItemByCategoryId(value) : findTimeSlotByValue(value));
-  }, [value, dataType, categories, findItemByCategoryId]);
+  }, [value, dataType, categories]);
 
   useOutsideClick(itemRef, toggleState, exceptionRef);
 
