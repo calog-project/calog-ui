44522336b63688ec56409eb3995bda9367f44980
junhyeon0218 <junhyeon0218@daum.net>
2025-04-03 00:26:44 +0900

    🐛 Fix: 충돌 오류 해결

--- src/components/domains/main/calendar/MainCalendar.tsx
+++ src/components/domains/main/calendar/MainCalendar.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import useCalendar from '@/hooks/useCalendar';
 import { DAY_LIST } from '@/constants/calendar';
 import { getDayClass } from '@/utils/calendar';
