489a85ebec7aa7a6983997f3a48c13cbc3c4e919
junhyeon0218 <junhyeon0218@gmail.com>
2025-11-13 22:47:42 +0900

    📦️ Chore: 테일윈드 수정 및 패키지 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,31 +6,32 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
     extend: {
       colors: {
         primary: '#112D4E',
         blue: {
           '3f': '#3F72AF',
-          76: '#76A5EA',
+          '76': '#76A5EA',
           db: '#DBE2EF',
-          33: '#3366FE',
-          41: '#4171FF',
+          '33': '#3366FE',
+          '41': '#4171FF',
         },
         gray: {
-          78: '#787486',
+          '78': '#787486',
           '9f': '#9FA6B2',
           d9: '#D9D9D9',
           ee: '#EEEEEE',
           fa: '#FAFAFA',
-          98: '#989898',
+          '98': '#989898',
           ef: '#EFEFEF',
           a9: '#A9A9A9',
         },
         black: {
           DEFAULT: '#000',
-          17: '#171717',
-          33: '#333236',
+          '17': '#171717',
+          '33': '#333236',
           '4b': '#4B4B4B',
           overlay: 'rgba(0, 0, 0, 0.70)',
         },
